8859937785998baf5777505ba93ac3dc95d8a8c4
slipperee_slime <scheidlercai@gmail.com>
2025-02-01 20:04:44 -0800

    ILT changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/customclasses/mechanisms/RRClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/customclasses/mechanisms/RRClaw.java
@@ -26,7 +26,7 @@ public class RRClaw extends RRMechanism {
 
 
         PRE_SAMPLE_DEPOSIT(SAMPLE_DEPOSIT_WRIST_POS, CLAW_GRABBING_POS),
-        PRE_SPECIMEN_DEPOSIT(STOWED_WRIST_POS, CLAW_GRABBING_POS);
+        PRE_SPECIMEN_DEPOSIT(SUBMERSIBLE_GRAB_WRIST_POS, CLAW_GRABBING_POS);
 
         float wristServoPos, clawServoPos, rotationServoPos;
         ClawPos(float wristServoPos, float clawServoPos, float rotationServoPos) {
@@ -44,10 +44,10 @@ public class RRClaw extends RRMechanism {
 
     private static final float STOWED_WRIST_POS = .66f;
     private static final float SAMPLE_DEPOSIT_WRIST_POS = 0.35f;
-    private static final float POST_GRAB_WRIST_POS = 0.2f;
+    private static final float POST_GRAB_WRIST_POS = 0.45f;
 
     private static final float SUBMERSIBLE_GRAB_WRIST_POS = 0.0f;
-    private static final float SPECIMEN_GRAB_WRIST_POS = 0.3f; //was .35f for all other meets || was .4 for Meet 2 || THIS MIGHT NEED TO BE CHANGED FOR OFF WALL GRABBING
+    private static final float SPECIMEN_GRAB_WRIST_POS = 0.35f; //was .35f for all other meets || was .4 for Meet 2 || THIS MIGHT NEED TO BE CHANGED FOR OFF WALL GRABBING
 
     private static final float CLAW_NOT_GRABBING_POS = 0.15f;
     private static final float CLAW_GRABBING_POS = 0.45f; //MAKE THIS MOREEEE
